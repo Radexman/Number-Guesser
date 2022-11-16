2eaa00bf9749c0334e6fcd4c0784c8e5fd038110
Radosław Siek <106437063+Radexman@users.noreply.github.com>
2022-11-16 13:52:12 +0100

    Update 3

--- js/main.js
+++ js/main.js
@@ -9,13 +9,14 @@ const guessField = document.querySelector('.guessField');
 
 let guessCount = 1;
 let resetButton;
+guessField.focus();
 
 function checkGuess() {
     const userGuess = Number(guessField.value);
     if (guessCount === 1) {
-        guesses.textContent = 'Previous guesses: ';
+         guess.textContent = 'Previous guesses: ';
     } 
-    guesses.textContent += `${userGuess}`;
+    guess.textContent += `${userGuess}`;
 
     if (userGuess === randomNumber) {
         lastResult.textContent = 'Congratulations! You got it right';
@@ -40,3 +41,34 @@ function checkGuess() {
     guessField.value = '';
     guessField.focus();
 }
+
+guessSubmit.addEventListener('click', checkGuess);
+
+function setGameOver() {
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Start new game';
+    document.body.append(resetButton);
+    resetButton.addEventListener('click', resetGame);
+  }
+
+  function resetGame() {
+    guessCount = 1;
+  
+    const resetParas = document.querySelectorAll('.resultParas p');
+    for (const resetPara of resetParas) {
+      resetPara.textContent = '';
+    }
+  
+    resetButton.parentNode.removeChild(resetButton);
+  
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = '';
+    guessField.focus();
+  
+    lastResult.style.backgroundColor = 'green';
+  
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+  }
